765f77cc3fd36bfea8125d972d9800676ab1dddd
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2021-12-19 14:37:47 +0100

    Submission of day 01 part 2 solution

--- 2021/01_day.rb
+++ 2021/01_day.rb
@@ -3,18 +3,39 @@ class DepthMeasure
     new(input).increase_count
   end
 
+  def self.group_increases(input)
+    new(input).group_increase_count
+  end
+
   def initialize(input)
     @measurements = input.lines.map(&:to_i)
   end
 
-  def increase_count
+  def increase_count(array: @measurements)
     increases = 0
     previous = nil
-    @measurements.each do |x|
+    array.each do |x|
       increases += 1 if !previous.nil? && x > previous
       previous = x
     end
 
     increases
+  end
+
+  def group_increase_count
+    increase_count(array: threes)
+  end
+
+  def threes
+    groups_of_three = []
+    last_three = []
+    @measurements.each do |x|
+      last_three << x
+      if last_three.length == 3
+        groups_of_three << last_three.sum
+        last_three = last_three[1..2]
+      end
+    end
+    groups_of_three
   end
 end
--- 2021/01_day_test.rb
+++ 2021/01_day_test.rb
@@ -2,19 +2,24 @@ require 'minitest/autorun'
 require_relative '01_day'
 
 class DepthMeasureTest < Minitest::Test
-  def test_initial_data
-    measurements = "199
-    200
-    208
-    210
-    200
-    207
-    240
-    269
-    260
-    263"
+  INITIAL_MEASUREMENTS = "199
+  200
+  208
+  210
+  200
+  207
+  240
+  269
+  260
+  263"
 
-    increase_count = DepthMeasure.increases(measurements)
+  def test_single_increases_for_initial_data
+    increase_count = DepthMeasure.increases(INITIAL_MEASUREMENTS)
     assert_equal increase_count, 7
+  end
+
+  def test_group_increases_for_initial_data
+    group_increase_count = DepthMeasure.group_increases(INITIAL_MEASUREMENTS)
+    assert_equal group_increase_count, 5
   end
 end
